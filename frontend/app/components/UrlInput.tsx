2f109da86c3a18a68bbef454d6f71f8f6456ed15
jamiegyoung <jamiegyoung22@gmail.com>
2023-10-16 23:26:58 +0100

    Update UrlInput add index and numberToOrdinal for input title

--- frontend/app/components/UrlInput.tsx
+++ frontend/app/components/UrlInput.tsx
@@ -1,7 +1,40 @@
-export default function UrlInput() {
+export default function UrlInput({ index }: { index: number }) {
+  const numberToOrdinal = (number: number): string => {
+    if (number < 0 || number > 1000) {
+      return "Number out of range (0-1000)";
+    }
+
+    if (number === 0) return "zero";
+
+    const onesCardinal = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const tensCardinal = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const onesOrdinal = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth'];
+    const teensOrdinal = ['eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth'];
+    const tensOrdinal = ['', 'tenth', 'twentieth', 'thirtieth', 'fortieth', 'fiftieth', 'sixtieth', 'seventieth', 'eightieth', 'ninetieth'];
+
+    if (number === 1000) return 'one-thousandth'
+
+    if (number % 100 === 0) return `${onesCardinal[Math.floor(number / 100)]}-hundredth`
+
+    if (number > 100) return `${onesCardinal[Math.floor(number / 100)]}-hundred and ${numberToOrdinal(number - (Math.floor(number / 100) * 100))}`
+
+    if (number % 10 === 0) return tensOrdinal[Math.floor(number / 10)];
+
+    if (number < 10) return onesOrdinal[number - 1];
+
+    if (number >= 11 && number <= 19) return teensOrdinal[number - 11];
+
+    return `${tensCardinal[Math.floor(number / 10)]}-${onesOrdinal[number % 10 - 1]}`;
+  }
+
   return (
     <div>
-      <div className="font-black">first url</div>
+      <div className="w-full items-center flex flex-col mb-8">
+        <div className="max-w-xl w-full">
+          <div className="font-black ml-4 mb-0">{numberToOrdinal(index)} url</div>
+        </div>
+        <input className="max-w-xl w-full font-bold shadow-hard bg-background-light text-foreground rounded-tl-2xl rounded-br-2xl p-2" />
+      </div>
     </div>
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <h1 className="font-black text-4xl mb-8">randirect</h1>
-      <div className="flex-grow bg-background rounded-2xl p-10 shadow-xl">
-        <UrlInput />
+      <div className="max-w-xl w-full justify-center flex-grow bg-background rounded-2xl p-10 shadow-xl">
+        {
+          Array.from({ length: 1000 }, (_, index) => index).map((i) => <UrlInput index={i + 1} />)
+        }
       </div>
     </main>
   )
